fa6f3687540cd1c4b007a1a63218c5afc6dc1bd2
khanna <gautam_khna@yahoo.com>
2020-04-25 02:26:48 +0100

    Weather Api service running code.

---
+++ src/test/java/com/khanna/weather/business/service/WeatherApiServiceTest.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package com.khanna.weather.business.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.UnsupportedEncodingException;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+/**
+ * @author khanna
+ *
+ */
+@SpringBootTest
+public class WeatherApiServiceTest {
+	
+	Logger logger =LoggerFactory.getLogger(WeatherApiServiceTest.class);
+	@Autowired
+	private WeatherApiService weatherApiService;
+	
+	@Test
+	public void getCountryWeatherWithValidValue() {
+		String countryName= "singapore";
+		try {
+			ResponseEntity<String> response = weatherApiService.getCountryWeather(countryName);
+			logger.info(response.getBody());
+			assertEquals(200, response.getStatusCodeValue());
+		} catch (UnsupportedEncodingException e) {
+			logger.error(e.toString());
+			
+		}
+	}
+	
+//	@Test
+//	public void getCountryWeatherWithNullValue() {
+//		String countryName= "null";
+//		try {
+//			ResponseEntity<String> response = weatherApiService.getCountryWeather(countryName);
+//			logger.info(response.getBody());
+//			assertEquals(200, response.getStatusCodeValue());
+//		} catch (UnsupportedEncodingException e) {
+//			logger.error(e.toString());
+//			
+//		}
+//	}
+}
